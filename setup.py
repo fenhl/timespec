a45b6f76f6288b7e4279e39ba50115badc3ed75c
Lothar Komas <thomaskolar90@gmail.com>
2017-01-25 23:16:31 +0100

    removed comments as requested

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ setup(name='timespec',
         description='Quickly specify dates and times',
         author='Fenhl',
         packages=["timespec"],
-        # use_scm_version = {
-        #     "write_to": "backuproll/_version.py",
-        # },
         install_requires=[
             "pytz",
         ]
